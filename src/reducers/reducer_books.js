ca717aae728760e9fbf6e31d93c09c7d7b434d38
Sahil <sahil.maheshwari@oracle.com>
2018-08-08 15:03:01 -0700

    Added Redux BookList Code

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+  return [
+    // eslint-disable-next-line
+    { title: 'Javascript: The Good Parts', pages:101},
+    { title: 'Happy Potter', pages:30},
+    { title: 'The Lean Startup', pages:123},
+    { title: 'Extreme Ownership', pages:158}
+  ]
+}
